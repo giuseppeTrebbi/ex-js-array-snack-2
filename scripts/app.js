2a85ed5b15a296d6b535908d097900c931bfc163
giuseppeTrebbi <giuseppe.trebbi97@gmail.com>
2026-05-06 15:41:05 +0200

    feat: project's over

--- scripts/app.js
+++ scripts/app.js
@@ -54,7 +54,7 @@ console.log("Titoli:", longBooksTitles)
 const availableBooks = books.filter(libro => libro.available)
 const discountedBooks = availableBooks.map(libro => {
     const prezzo = (Number(libro.price.replace("€", "")) * 4 / 5).toFixed(2) + "€"
-    return {...libro, price: prezzo}
+    return { ...libro, price: prezzo }
 })
 const fullPricedBooks = discountedBooks.find(libro => {
     const decimali = Number(libro.price.replace("€", "")) - Number(Number(libro.price.replace("€", "")).toFixed(0))
@@ -66,7 +66,7 @@ console.log("Prezzo intero:", fullPricedBooks)
 //ex3
 const authors = books.map(libro => libro.author)
 const areAuthorsAdult = authors.every(autore => autore.age >= 18)
-if(areAuthorsAdult) {
+if (areAuthorsAdult) {
     authors.sort((a, b) => a.age - b.age)
 } else {
     authors.sort((a, b) => b.age - a.age)
@@ -76,8 +76,49 @@ console.log("Autori ordinati:", authors)
 
 //ex4
 const ages = authors.map(autore => autore.age)
-const agesSum = ages.reduce((somma, età) => somma += età , 0)
+const agesSum = ages.reduce((somma, età) => somma += età, 0)
 console.log("Età media:", agesSum / ages.length)
+
+
+//ex5
+function getBooks(ids) {
+    const promesseLibri = ids.map(id => new Promise(resolve => resolve(fetch(`http://localhost:3333/books/${id}`).then(data => data.json()))))
+    return Promise.all(promesseLibri)
+}
+getBooks([2, 13, 7, 21, 19]).then(libri => {
+    console.log("Libri da API:", libri)
+})
+
+
+//ex6
+const areThereAvailableBooks = books.some(libro => libro.available)
+const booksByPrice = books.toSorted((libroA, libroB) => Number(libroA.price.replace("€", "")) - Number(libroB.price.replace("€", "")))
+booksByPrice.sort((libroA, libroB) => libroA.available ? -1 : 1)
+console.log("Libri ordinati:", booksByPrice)
+
+
+//ex7
+const tags = []
+const tagCounts = []
+
+books.forEach(libro => {
+    libro.tags.forEach(tag => {
+        if (!tags.includes(tag)) {
+            tags.push(tag)
+        }
+    })
+})
+tags.forEach(tag => {
+    const count = books.reduce((acc, libro) => {
+        if (libro.tags.includes(tag)) {
+            acc++
+        }
+        return acc
+    }, 0)
+    tagCounts.push({ tag: tag, frequenza: count })
+})
+console.log("Tags count:", tagCounts)
+
 
 
 
